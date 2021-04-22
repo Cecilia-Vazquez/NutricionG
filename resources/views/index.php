78697d2b3287ef1e5bbb790ea0b00ae816b2139f
Fren Rosita Rossita <A198933@alumnos.uaslp.mx>
2021-04-22 10:23:04 -0500

    cascaron listo

---
+++ resources/views/index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>.::. Nutricion Genetica .::. </title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+   
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <!-- Iconos -->
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+
+    <!--estilos css 15-19 abril 2021 rimo-->
+   <!--<link rel="stylesheet" href="resources/css/estilos.css">--->
+   <link href="css/estilos.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <div class="container-fluid" >
+   <!-- Banner -->
+       <div class="row rowbannersuperior">
+        <div class="col-12">
+          <img src="img/bannersuperior.jpg" class="bannersuperior" alt="Responsive image"> 
+        </div> 
+      </div>
+    </div> <!--fin div container-->
+
+   <!-- style="background-color:#e6e6e6;"-->
+ 
+    <div class="container-fluid">
+
+    <nav class="navbar navbar-expand-lg navbar-dark fondoamarillo " >
+ 
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+    <ul class="navbar-nav mr-auto">
+    <!--MENÚ Inicio-->
+      <li class="nav-item active">
+        <a class="navbar-brand " href="index.php">
+          <i class="fa fa-home fa-fw" aria-hidden="true">
+            
+          </i>
+        <font color="red" size="4" face="Arial Black"> Inicio</font>
+       
+        </a>
+      </li>
+    <!--MENÚ ¿QUienes somos?-->
+      <li class="nav-item">
+        <a class="nav-link letra" href="QuienesSomos.php">
+           <font color="red" size="4" face="Arial Black"> 
+          ¿Quiénes Somos?
+            </font>
+        </a>
+      </li>
+    <!--MENÚ Inicio-->
+      <li class="nav-item">
+        <a class="nav-link " href="Mision.php"> 
+             <font color="red" size="4" face="Arial Black"> 
+         Misión
+            </font>
+        </a>
+      </li>
+<!--MENÚ mision-->
+     <li class="nav-item">
+        <a class="nav-link" href="Vision.php">
+           <font color="red" size="4" face="Arial Black"> 
+         Visión
+            </font>
+        </a>
+
+      </li>
+<!--MENÚ contacto-->
+      <li class="nav-item">
+        <a class="nav-link" href="Contacto.php">
+           <font color="red" size="4" face="Arial Black"> 
+         Contactános
+            </font>
+        </a>
+
+      </li>
+<!--MENÚ redes sociales-->
+      <li class="nav-item">
+        <a class="nav-link" href="RedesSociales.php">
+           <font color="red" size="4" face="Arial Black"> 
+         Nuestras Redes Sociales
+            </font>
+        </a>
+
+      </li>
+<!--MENÚ login-->
+      <li class="nav-item">
+        <a class="nav-link" href="Login.php">
+           <font color="red" size="4" face="Arial Black"> 
+         Login
+            </font>
+        </a>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
+
+<div class="card border-info rounded-0">
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/carousel1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block text-center " style=" background: rgba(0, 0, 255, 0.3)">
+        <h5 >Predicción</h5>
+        <p>Lunares benignos y malignos a través del uso de Redes Neuronales Profundas.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/carousel2.jpg" class="d-block w-100" >
+      <div class="carousel-caption d-none d-md-block" style=" background: rgba(0, 0, 255, 0.3)">
+        <h5>Lunares</h5>
+        <p>Se producen cuando las células productoras de pigmento de la piel, llamadas melanocitos, crecen en grupos</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/carousel3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block" style=" background: rgba(0, 0, 255, 0.3)">
+        <h5>Melanoma</h5>
+        <p>Una de las formas de cáncer más comunes, representa un riesgo grave para la salud  en todo el mundo.</p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+     </div>
+  <div class="container-fluid" style="background-color:#E9F6ED;">  
+    <div class="row justify-content-center">
+    <div class="col-md-12"> 
+    <br><br><br> 
+    <h1>Hola mundo</h1> 
+    <br><br>
+    </div>
+    </div>
+  </div>
+
+<!--Pie de pagina-->
+<div class="rowbannerinferior" style="background-color:rgba(254,0,0)">
+
+  <img src="img/bannerinferior.jpg " class="bannerinferior">
+
+      <small>
+        <p  class="card-text text-center" style=""> 
+          <font color="yellow" size="4" face="Arial Black"> Dirección 
+          </font>
+          <br>
+          <br>
+          <font color="yellow" size="4" face="Arial Black"> Teléfonos:     (444)  
+          </font>
+          
+          <br>
+          <font color="yellow" size="4" face="Arial Black"> 
+          <strong>Correo:</strong>
+          </font> 
+          <br>
+          <strong></strong>
+        </p>
+      </small>
+
+        <i class="fa fa-facebook-square fa-2x" style="color:#E0F0E0" aria-hidden="true"> 
+         </i> 
+        <i class="fa fa-linkedin-square fa-2x" style="color:#E0F0E0" aria-hidden="true">
+        </i>
+</div> <!----fin del div pie p+agina---->
+</div>
+</body>
+
+</html>
